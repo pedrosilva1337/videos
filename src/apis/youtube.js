c66c03cdc118bc18fdb9ed6f678301c8e431df47
Pedro Silva <pedroncsilva26@gmail.com>
2021-08-29 14:36:28 +0100

    Searching for Videos

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyDAjK2TmuRBFwX1Td1SYFKdRerZB4oz7MY";
+
+export default axios.create({
+	baseURL: "https://www.googleapis.com/youtube/v3",
+	params: {
+		part: "snippet",
+		maxResults: 5,
+		key: KEY,
+	},
+});
